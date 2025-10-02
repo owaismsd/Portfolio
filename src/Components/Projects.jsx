dc0a299c6eaf5dd6a72a2ba94d9caf3f02ca552e
Owais <owaisahmad.maseed@gmail.com>
2025-10-02 20:29:53 +0500

    Project Completed Bro Just text is remaining to chage

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -11,13 +11,13 @@ function Projects() {
         </div>
         <div className="main grid grid-cols-1 md:grid-cols-2 gap-6 md:w-[90%] ">
         <div className="mini-card border-[#292929] border-2 bg-[#101010] rounded-xl p-4 mt-5">
-          <img src="prj.avif" className="rounded-lg" alt="" />
+          <img src="WebProject1.png" className="rounded-lg" alt="" />
 
           <div className="flex items-center justify-between">
             <div>
-              <h1 className="text-xl mt-4">HelloBot</h1>
+              <h1 className="text-xl mt-4">Hero Page</h1>
               <p className="text-[#c5c5c5] text-sm pt-1">
-                SaaS & Startup · 8 Pages
+                React JS • CSS · 1 Page
               </p>
             </div>
             <button className="ml-4 bg-[#1f1f1f] p-3 rounded-4xl mt-5 text-[#cccccc]"><i class="fa-solid fa-up-right-from-square"></i></button>
@@ -26,13 +26,13 @@ function Projects() {
 
         </div>
         <div className="mini-card border-[#292929] border-2 bg-[#101010] rounded-xl p-4 mt-4">
-          <img src="prj.avif" className="rounded-lg" alt="" />
+          <img src="weatherapp.png" className="rounded-lg" alt="" />
 
           <div className="flex items-center justify-between">
             <div>
-              <h1 className="text-xl mt-4">HelloBot</h1>
+              <h1 className="text-xl mt-4">Weather App</h1>
               <p className="text-[#c5c5c5] text-sm pt-1">
-                SaaS & Startup · 8 Pages
+              React JS • API • Tailwind · 2 Pages
               </p>
             </div>
             <button className="ml-4 bg-[#1f1f1f] p-3 rounded-4xl mt-5 text-[#cccccc]"><i class="fa-solid fa-up-right-from-square"></i></button>
@@ -41,13 +41,13 @@ function Projects() {
 
 
         <div className="mini-card border-[#292929] border-2 bg-[#101010] rounded-xl p-4 mt-4">
-          <img src="prj.avif" className="rounded-lg" alt="" />
+          <img src="ilmaarawebsite.png" className="rounded-lg" alt="" />
 
           <div className="flex items-center justify-between">
             <div>
-              <h1 className="text-xl mt-4">HelloBot</h1>
+              <h1 className="text-xl mt-4">Ilmaara Website</h1>
               <p className="text-[#c5c5c5] text-sm pt-1">
-                SaaS & Startup · 8 Pages
+                HTML • CSS • JS · 12 Pages
               </p>
             </div>
             <button className="ml-4 bg-[#1f1f1f] p-3 rounded-4xl mt-5 text-[#cccccc]"><i class="fa-solid fa-up-right-from-square"></i></button>
